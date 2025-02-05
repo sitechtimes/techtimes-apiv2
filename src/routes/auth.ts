1f932675fb1aaff4c2fe37b452433afe41b47ece
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2025-02-05 01:03:12 -0500

    subuluwa

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,7 +3,6 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 import { body } from "express-validator";
 import { validateRequest } from "../utils/requestValidator";
-import { currentUser } from "../utils/currentUser";
 
 router.post(
   "/signup",
