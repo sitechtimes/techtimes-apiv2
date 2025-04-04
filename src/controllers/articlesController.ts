00220e14e43558b6906852055b12310eca464536
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2025-04-04 11:09:07 -0400

    stop that actually

--- src/controllers/articlesController.ts
+++ src/controllers/articlesController.ts
@@ -15,7 +15,6 @@ async function homepage(req: Request, res: Response) {
     .select("-content")
     .sort({ updatedAt: "descending" })
     .limit(20);
-  console.log(homepages);
   res.status(200).send(homepages);
 }
 
