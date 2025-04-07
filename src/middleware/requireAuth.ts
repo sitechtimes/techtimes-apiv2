5941712257cea29d4368941b29f57c79061aa59c
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2025-04-06 21:40:37 -0400

    wait don't laugh at the user

--- src/middleware/requireAuth.ts
+++ src/middleware/requireAuth.ts
@@ -1,10 +1,7 @@
 import { Response, NextFunction, Request } from "express";
 
 export const requireAuth = (req: Request, res: Response, next: NextFunction) => {
-  if (!req.currentUser) {
-    res.status(401).json({ message: "Unauthorized (haha)" });
-    return;
-  }
+  if (!req.currentUser) return void res.status(401).json({ message: "Unauthorized" });
 
   next();
 };
