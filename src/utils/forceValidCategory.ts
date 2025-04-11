fb315cd520bf105e7500634cd55649f4a5e1be74
NicoleSoltsov <113133856+NicoleSoltsov@users.noreply.github.com>
2025-04-11 11:00:07 -0400

    fdf

--- src/utils/forceValidCategory.ts
+++ src/utils/forceValidCategory.ts
@@ -2,13 +2,20 @@ import { Article } from "../models/article";
 import { Category } from "../models/category";
 import { Draft } from "../models/cms/draft";
 
+function isValidCategory(value: unknown): value is Category {
+  return Object.values(Category).includes(value as Category);
+}
+
 export const forceValidCategory = async (id: string) => {
   const target = (await Article.findById(id)) || (await Draft.findById(id));
   if (!target) return;
+
   console.log(`looking at "${target.title}"'s category...`);
-  if (!Object.values(Category).includes(target.category)) {
+
+  if (!isValidCategory(target.category)) {
     target.category = Object.values(Category)[0];
     console.log(`category was invalid. changed to ${target.category}`);
   }
+
   await target.save();
 };
