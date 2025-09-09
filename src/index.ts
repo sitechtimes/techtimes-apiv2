9565e9c35faaec4d882522f8f97fdfa2c64843e7
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-09-09 10:54:33 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ app.use(
       "http://localhost:8089",
       "http://localhost:8000",
       "http://100.101.70.222:8089",
+      "http://100.101.65.65:8089",
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
