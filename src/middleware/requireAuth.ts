40805fd5b6c9240d4ede0871bb6bb563c6147cbe
kongeebowl <kadenpang09@gmail.com>
2025-10-07 10:49:59 -0400

    NO MROE CONSOLE LOG

--- src/middleware/requireAuth.ts
+++ src/middleware/requireAuth.ts
@@ -3,12 +3,8 @@ import { User } from "../models/user";
 
 /** checks that the user's JWT is valid, and that they are verified */
 export const requireAuth = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(1);
   if (!req.currentUser) return void res.sendStatus(401);
-  console.log(2);
   const user = await User.findById(req.currentUser.id);
-  console.log(3);
   if (!user || !user.verified) return void res.sendStatus(401);
-  console.log(4);
   next();
 };
