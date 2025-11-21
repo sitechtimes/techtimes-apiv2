6726b8b06c2555f2831ee4987013c836c8afeecd
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2025-11-20 21:40:07 -0500

    it works now

--- src/models/cms/draft.ts
+++ src/models/cms/draft.ts
@@ -9,6 +9,7 @@ interface DraftAttrs {
   customAuthor?: string;
   imageUrl?: string;
   imageAlt?: string;
+  editorResponses?: string[];
   status?: DraftStatus;
   category?: Category;
 }
@@ -20,6 +21,7 @@ export interface DraftDoc extends mongoose.Document {
   customAuthor?: string;
   imageUrl: string;
   imageAlt: string;
+  editorResponses: string[];
   status: DraftStatus;
   category: Category;
 }
@@ -59,11 +61,11 @@ const draftSchema = new mongoose.Schema(
       required: false,
       trim: true,
     },
-    editorResponses: [{
-      type: String,
+    editorResponses: {
+      type: [Object.values(String)],
       default: [],
       required: false,
-    }],
+    },
     status: {
       type: String,
       enum: Object.values(DraftStatus),
