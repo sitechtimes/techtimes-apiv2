f013b46fc1acc860a0ee1861f72d6ccdbca26095
Constantine Nagornuk <conplay55@gmail.com>
2025-12-10 10:43:54 -0500

    Fixed logic
    My dumb ahh was looking at the wrong var half the time |
    Onto the sorting and pages

--- src/utils/monthlyViewReset.ts
+++ src/utils/monthlyViewReset.ts
@@ -3,41 +3,34 @@ const { DateTime } = require("luxon");
 const currentDate = DateTime.now().toObject();
 var resetTime: boolean = false;
 var resetDone: boolean = false;
-console.log(currentDate);
 
 function monthCheck() {
   const currentDay = currentDate.day;
-  const resetDay: number = 9;
+  const resetDay: number = 1;
   if (currentDay === resetDay && resetDone === false) {
     resetTime = true;
-    console.log("1");
     return resetTime;
   } else if (currentDay === resetDay && resetDone === true) {
     resetTime = false;
-    console.log("2");
     return resetTime;
   } else if (currentDay !== resetDay) {
     /* this whole function has to run every day then somewhere in order for the logic to work */
     resetDone = false;
     resetTime = false;
-    console.log("3");
     return [resetTime, resetDone];
   } else {
     resetTime = false;
-    console.log("4");
     return resetTime;
   }
 }
 
 export async function resetMonthlyViews() {
-  monthCheck();
-  if (resetTime === true && resetDone === false) {
+  if (!monthCheck()) return null;
+  try {
+    const result = await Article.updateMany({}, { $set: { viewCountMonthly: 0 } }).exec();
     resetDone = true;
-    await Article.updateMany({}, { $set: { viewCountMonthly: 0 } });
-    console.log("TESTTTTTT");
-    return resetDone;
-  } else {
-    console.log("HELLP");
-    null;
+    return result;
+  } catch (err) {
+    throw err;
   }
 }
