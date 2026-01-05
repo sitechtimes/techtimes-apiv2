a9ff67a14143b1e4bf2b7f84159b92af11a4ba06
Constantine Nagornuk <conplay55@gmail.com>
2026-01-05 10:39:13 -0500

    Previous Month trending system WIP 0.3.4
    Works.
    Will test again to see if it holds up the reset logic

--- src/utils/monthlyViewReset.ts
+++ src/utils/monthlyViewReset.ts
@@ -3,7 +3,7 @@ const { DateTime } = require("luxon");
 const currentDate = DateTime.now().toObject();
 export let resetTime: boolean = false;
 export let resetDone: boolean = false;
-export const resetDay: number = 22;
+export const resetDay: number = 5;
 
 function monthCheck() {
   const currentDay = currentDate.day;
