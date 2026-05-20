7b729f30921aef339a1f46480c7f0c0da4f07221
pichu1298 <kristinachuchu@gmail.com>
2026-05-20 10:38:26 -0400

    ?

--- src/routes/crosswords.ts
+++ src/routes/crosswords.ts
@@ -1,8 +1,7 @@
 import express from "express";
-import { requireAuth } from "../middleware/requireAuth";
 const router = express.Router();
 
 const crosswordController = require("../controllers/crosswordController");
 
-router.post("/crosswords", requireAuth, crosswordController.createCrossword);
+router.post("/crosswords", crosswordController.createCrossword);
 router.get("/crosswords", crosswordController.getCrosswordForToday);
